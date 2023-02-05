b535044903c8b239cf2d3cd8202a240baa984708
Michał Sadurski <mcplay7@gmail.com>
2023-02-05 21:39:38 +0100

    Initial Commit

---
+++ HoleDetector/HoleDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoleDetectorApp.swift
+//  HoleDetector
+//
+//  Created by Michał Sadurski on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoleDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
